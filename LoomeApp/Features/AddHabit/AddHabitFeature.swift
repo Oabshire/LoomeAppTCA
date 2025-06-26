81b23239ccbf7117008709bbe2186823276bf330
Anastasiia Tereshchenko <ms.staisy@gmail.com>
2025-06-26 13:38:44 -0700

    5 add delegate  and dismiss to AddHabitFeature

--- LoomeApp/Features/AddHabit/AddHabitFeature.swift
+++ LoomeApp/Features/AddHabit/AddHabitFeature.swift
@@ -10,28 +10,41 @@ import ComposableArchitecture
 
 @Reducer
 struct AddHabitFeature {
-  @ObservableState
-  struct State: Equatable {
-    var habit: Habit
-  }
-  enum Action {
-    case cancelButtonTapped
-    case saveButtonTapped
-    case setName(String)
-  }
-  var body: some ReducerOf<Self> {
-    Reduce { state, action in
-      switch action {
-      case .cancelButtonTapped:
-        return .none
-
-      case .saveButtonTapped:
-        return .none
-
-      case let .setName(name):
-        state.habit.name = name
-        return .none
-      }
+    @ObservableState
+    struct State: Equatable {
+        var habit: Habit
     }
-  }
+    enum Action {
+        case delegate(Delegate)
+        case cancelButtonTapped
+        case saveButtonTapped
+        case setName(String)
+        enum Delegate: Equatable {
+            case saveHabit(Habit)
+        }
+    }
+
+    @Dependency(\.dismiss) var dismiss
+
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .delegate:
+                return .none
+
+            case .cancelButtonTapped:
+                return .run { _ in await self.dismiss() }
+
+            case .saveButtonTapped:
+                return .run { [habit = state.habit] send in
+                    await send(.delegate(.saveHabit(habit)))
+                    await self.dismiss()
+                }
+
+            case let .setName(name):
+                state.habit.name = name
+                return .none
+            }
+        }
+    }
 }
